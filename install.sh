b14e9c2f01b65bd6035b6b9bd60928cf167c821c
vagrant <vagrant@homestead>
2017-01-13 15:03:44 +0000

    project base

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
+#créer un nouveau dossier
+#faire un vagrant provision
+#rajouter à l'intérieur : composer.json, install.sh, package.json
+#git init
+#créer un dossier .gitignore
+#installer le tout avec "sh install.sh"
+
 composer install
 npm install
 mkdir view
