d18922f237f2ae4197234976239bc1f352707c30
vagrant <vagrant@homestead>
2017-01-17 17:46:02 +0000

    navbar finale

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,4 +1,6 @@
 (function (){
+  const overlay = document.querySelector('#overlay');
+
 
   let sidebarOpened = false
   let button = document.querySelector('#menu')
@@ -6,23 +8,35 @@
   button.addEventListener('click',function (e){
     e.stopPropagation()
     e.preventDefault()
-    document.body.classList.add('has-sidebar')
-    sidebarOpened = true
-  })
-
-  document.body.addEventListener('click', function (){
+    document.body.classList.toggle('has-sidebar')
+    overlay.classList.toggle('is-hide');
     if(sidebarOpened){
-      document.body.classList.remove('has-sidebar')
+      sidebarOpened = false;
+    }else{
+      sidebarOpened = true;
     }
   })
 
+  overlay.addEventListener('click', function (){
+    if(sidebarOpened){
+      document.body.classList.remove('has-sidebar')
+      overlay.classList.add('is-hide');
+      sidebarOpened = false
+    }
+  })
+
+  let width;
+
+  window.addEventListener("resize", function(){
+    width = document.body.clientWidth;
+    if(width>680 && sidebarOpened){
+      document.body.classList.remove('has-sidebar')
+      overlay.classList.add('is-hide');
+    }
+    if(width<680 && sidebarOpened){
+      document.body.classList.add('has-sidebar')
+      overlay.classList.remove('is-hide');
+    }
+  }, true);
+
 })()
-
-var width;
-
-window.addEventListener("resize", function(){
-  width = document.body.clientWidth;
-  if(width>680){
-    document.body.classList.remove('has-sidebar')
-  }
-}, true);
